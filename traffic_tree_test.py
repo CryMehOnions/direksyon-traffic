623d7a2a22f84efce67e43c327bf35a3f622b6e4
Nate3447 <nathaniel.sierras@gmail.com>
2017-03-24 15:48:55 +0800

    Add files via upload

---
+++ traffic_tree_test.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect("host='128.199.106.13' dbname='mmda_traffic' user='direksyon' host='localhost' password='gothere4lyf'")
+    print("Connection successful")
+except:
+    print("DB connect failed.")
+
+cur = conn.cursor()
+
+# check when last update was
+#
+
+try:
+    cur.execute("""SELECT location_road, location_bound, location_area, timestamp, traffic FROM entries WHERE update_timestamp > timestamp '2016-09-25 00:00:00'""")
+except:
+    print("Data retrieval failed.")
+
+data = cur.fetchall()
+
+for row in data:  # Street, Day of Week, Time Interval, Traffic Condition
+
+
+    # Result: row[0] = Street, row[1] = Day of Week, row[2] = Time Interval
+
+    print(row)
