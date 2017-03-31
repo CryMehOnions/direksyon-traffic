e1001be676cc972b3429284e8e80865d7ddb036f
Nate3447 <nathaniel.sierras@gmail.com>
2017-03-31 15:02:27 +0800

    Add files via upload

--- TrafficModel/traffic_tree_test.py
+++ TrafficModel/traffic_tree_test.py
@@ -17,7 +17,7 @@ def get_data():
     #
 
     try:
-        cur.execute("""SELECT location_road, location_bound, location_area, timestamp, traffic FROM entries WHERE update_timestamp > timestamp '2017-03-20 11:00:00' AND update_timestamp < timestamp '2017-03-20 13:00:00'""")
+        cur.execute("""SELECT location_road, location_bound, location_area, timestamp, traffic FROM entries WHERE update_timestamp > timestamp '2017-03-19 00:00:00' AND update_timestamp < timestamp '2017-03-21 00:00:00'""")
     except:
         print("Data retrieval failed.")
 
