378f1de9cba16a6f5b10e0f53f1c2dc709c43762
Miko Garcia <crymehonions@gmail.com>
2017-07-11 01:05:06 +0800

    try

--- TrafficModel/views.py
+++ TrafficModel/views.py
@@ -11,16 +11,13 @@ def getPrediction(request):
 	if request.method == 'GET':
 		form = PredictionForm(request)
 
-		if form.is_valid():
-			street = form.cleaned_data['street']
-			segment = form.cleaned_data['segment']
-			date = form.cleaned_data['date']
-			time = form.cleaned_data['time']
+		street = form.cleaned_data['street']
+		segment = form.cleaned_data['segment']
+		date = form.cleaned_data['date']
+		time = form.cleaned_data['time']
 
-			prediction = getPrediction(street, segment, date, time)
+		prediction = getPrediction(street, segment, date, time)
 
-			return HttpResponse(prediction)
-		else:
-			return HttpResponse("Something happened")
+		return HttpResponse(prediction)
 	else:
 		return HttpResponse("Something Happened at start")
