23a1141491dfa280c174e4b29ccfda75293d7715
Nate3447 <nathaniel.sierras@gmail.com>
2017-07-12 10:59:47 +0800

    Update db_test.py

--- TrafficModel/db_test.py
+++ TrafficModel/db_test.py
@@ -14,7 +14,7 @@ cur = conn.cursor()
 
 print("Querying database...")
 try:
-    cur.execute("""SELECT timestamp FROM entries WHERE update_timestamp > timestamp '2017-07-01 00:00:00' ORDER BY timestamp""")
+    cur.execute("""SELECT timestamp FROM entries WHERE update_timestamp > timestamp '2017-06-01 00:00:00' ORDER BY timestamp""")
 except:
     print("Data retrieval failed.")
 
